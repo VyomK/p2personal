9441870a53d33f32bc8248d0a47b6ecbb45fe8f6
Vyom K <kvyom24@gmail.com>
2025-05-19 16:24:32 +1000

    feat(wrappers/): added wrappers folder
    created memory.h/.c files
    
    This folder will contain wrapper functions
    for common functions with included error checking
    logic.
    Files will be named as per usage
    (Eg: memory.c/.h for heap memory fucntions).
    
    Naming convention for wrapper functions will
    follow capitalized names (malloc -> Malloc).

---
+++ wrappers/memory.c
@@ -0,0 +1,13 @@
+#include "memory.h"
+#include <stddef.h>
+
+
+
+
+void *Calloc(size_t nmemb, size_t size)
+{
+
+
+    /*implement*/
+    return calloc(nmemb, size);
+}
---
+++ wrappers/memory.h
@@ -0,0 +1,13 @@
+#ifdef MEMORY_WRAPPERS
+#define MEMORY_WRAPPERS
+
+#include <stdlib.h>
+
+
+
+void *Calloc(size_t nmemb, size_t size);
+
+
+
+
+#endif
