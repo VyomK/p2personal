13b02e1252061bed5192a519c28fe8b1758a0a56
Vyom K <kvyom24@gmail.com>
2025-06-04 00:26:29 +1000

    feat: completed server.c and client.c

--- source/client.c
+++ source/client.c
@@ -1,1 +1,264 @@
-//TODO: client code that can send instructions to server.
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <pthread.h>
+
+#include "ipc_helpers.h"
+#include "markdown.h"
+
+#define FIFO_NAME_MAX 256
+#define BUF_SIZE 4096
+
+document *local_doc = NULL;
+char *local_log = NULL;
+char *permission = NULL;
+
+pthread_mutex_t local_doc_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t local_log_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int fd_c2s = -1;
+int fd_s2c = -1;
+
+void *pipe_listener_thread(void *arg);
+void client_apply_broadcast(const char *msg);
+
+void cleanup_client(void);
+void client_handshake(pid_t server_pid, const char *username);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <server_pid> <username>\n", argv[0]);
+        return 1;
+    }
+
+    pid_t server_pid = (pid_t)atoi(argv[1]);
+    const char *username = argv[2];
+
+    client_handshake(server_pid, username);
+
+    pthread_t listener_thread;
+    pthread_create(&listener_thread, NULL, pipe_listener_thread, NULL);
+    pthread_detach(listener_thread);
+
+    char line[512];
+    while (fgets(line, sizeof(line), stdin))
+    {
+        if (strcmp(line, "DOC?\n") == 0)
+        {
+            pthread_mutex_lock(&local_doc_mutex);
+            markdown_print(local_doc, stdout);
+            pthread_mutex_unlock(&local_doc_mutex);
+            continue;
+        }
+        else if (strcmp(line, "LOG?\n") == 0)
+        {
+            pthread_mutex_lock(&local_log_mutex);
+            printf("%s", local_log ? local_log : "");
+            pthread_mutex_unlock(&local_log_mutex);
+            continue;
+        }
+        else if (strcmp(line, "PERM?\n") == 0)
+        {
+            printf("%s", permission ? permission : "(no permission info)\n");
+            continue;
+        }
+        else if (strcmp(line, "DISCONNECT\n") == 0)
+        {
+            dprintf(fd_c2s, "DISCONNECT\n");
+            break;
+        }
+
+        // Send to server
+        if (write(fd_c2s, line, strlen(line)) < 0)
+        {
+            perror("Failed to send command to server");
+            break;
+        }
+    }
+
+    cleanup_client();
+    return 0;
+}
+
+void client_handshake(pid_t server_pid, const char *username)
+{
+    char fifo_c2s[FIFO_NAME_MAX];
+    char fifo_s2c[FIFO_NAME_MAX];
+    pid_t client_pid = getpid();
+
+    // Block SIGRTMIN+1
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGRTMIN + 1);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+
+    // Send SIGRTMIN to server
+    kill(server_pid, SIGRTMIN);
+
+    // Wait for SIGRTMIN+1
+    int sig;
+    sigwait(&set, &sig);
+
+    // FIFO names
+    snprintf(fifo_c2s, sizeof(fifo_c2s), "FIFO_C2S_%d", client_pid);
+    snprintf(fifo_s2c, sizeof(fifo_s2c), "FIFO_S2C_%d", client_pid);
+
+    // Open FIFOs
+    fd_c2s = open(fifo_c2s, O_WRONLY);
+    if (fd_c2s < 0)
+    {
+        perror("open FIFO_C2S failed");
+        exit(1);
+    }
+
+    fd_s2c = open(fifo_s2c, O_RDONLY);
+    if (fd_s2c < 0)
+    {
+        perror("open FIFO_S2C failed");
+        close(fd_c2s);
+        exit(1);
+    }
+
+    // Send username
+    dprintf(fd_c2s, "%s\n", username);
+
+    // Read role line
+    char *role_line = read_line_dynamic(fd_s2c);
+    if (!role_line)
+    {
+        fprintf(stderr, "Failed to read role line\n");
+        cleanup_client();
+        exit(1);
+    }
+
+    if (strcmp(role_line, "Reject UNAUTHORISED") == 0)
+    {
+        fprintf(stderr, "Server rejected user\n");
+        free(role_line);
+        cleanup_client();
+        exit(1);
+    }
+
+    permission = strdup(role_line);
+    free(role_line);
+
+    // Read version and ignore
+    char *ver_line = read_line_dynamic(fd_s2c);
+    if (!ver_line)
+    {
+        fprintf(stderr, "Failed to read version line\n");
+        cleanup_client();
+        exit(1);
+    }
+    free(ver_line);
+
+    // Read document length
+    char *len_line = read_line_dynamic(fd_s2c);
+    if (!len_line)
+    {
+        fprintf(stderr, "Failed to read length line\n");
+        cleanup_client();
+        exit(1);
+    }
+    size_t doc_len = (size_t)strtoull(len_line, NULL, 10);
+    free(len_line);
+
+    // Read document content
+    char *buffer = Calloc(doc_len + 1, sizeof(char));
+    size_t total_read = 0;
+    while (total_read < doc_len)
+    {
+        ssize_t n = read(fd_s2c, buffer + total_read, doc_len - total_read);
+        if (n <= 0)
+        {
+            fprintf(stderr, "Failed to read document content\n");
+            free(buffer);
+            cleanup_client();
+            exit(1);
+        }
+        total_read += n;
+    }
+    buffer[doc_len] = '\0';
+
+    local_doc = markdown_init();
+    markdown_parse_string(local_doc, buffer);
+    free(buffer);
+}
+
+void *pipe_listener_thread(void *arg)
+{
+    char buffer[BUF_SIZE];
+    size_t buf_len = 0;
+
+    while (1)
+    {
+        ssize_t n = read(fd_s2c, buffer + buf_len, BUF_SIZE - buf_len);
+        if (n <= 0)
+        {
+            fprintf(stderr, "Server connection lost.\n");
+            exit(1);
+        }
+
+        buf_len += n;
+        buffer[buf_len] = '\0';
+
+        // Wait until END\n to process broadcast
+        char *end_ptr = strstr(buffer, "END\n");
+        if (!end_ptr)
+            continue;
+
+        size_t full_len = end_ptr - buffer + 4;
+
+        // Extract broadcast string
+        char *broadcast = Calloc(full_len + 1, sizeof(char));
+        memcpy(broadcast, buffer, full_len);
+        broadcast[full_len] = '\0';
+
+        // Append to local_log
+        pthread_mutex_lock(&local_log_mutex);
+        size_t old_log_len = local_log ? strlen(local_log) : 0;
+        local_log = realloc(local_log, old_log_len + full_len + 1);
+        memcpy(local_log + old_log_len, broadcast, full_len);
+        local_log[old_log_len + full_len] = '\0';
+        pthread_mutex_unlock(&local_log_mutex);
+
+       pthread_mutex_lock(&local_doc_mutex);
+        apply_broadcast(broadcast);
+        markdown_increment_version(local_doc);
+        pthread_mutex_unlock(&local_doc_mutex);
+
+        // Shift remainder of buffer
+        size_t leftover = buf_len - full_len;
+        memmove(buffer, buffer + full_len, leftover);
+        buf_len = leftover;
+        buffer[buf_len] = '\0';
+
+        free(broadcast);
+    }
+
+    return NULL;
+}
+
+// === Cleanup ===
+void cleanup_client(void)
+{
+    if (fd_c2s >= 0)
+        close(fd_c2s);
+    if (fd_s2c >= 0)
+        close(fd_s2c);
+    if (permission)
+        free(permission);
+    if (local_log)
+        free(local_log);
+    if (local_doc)
+        markdown_free(local_doc);
+}
--- source/server.c
+++ source/server.c
@@ -1,1 +1,242 @@
-// TODO: server code that manages the document and handles client instructions
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+
+#include "markdown.h"
+#include "document.h"
+#include "array_list.h"
+#include "ipc_helpers.h"
+
+#define MAX_FIFO_NAME 64
+
+document *global_doc = NULL;
+pthread_mutex_t doc_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+array_list *connected_clients;
+pthread_mutex_t client_list_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+array_list *global_cmd_list;
+pthread_mutex_t cmd_list_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+char *server_log = NULL;
+size_t server_log_len = 0;
+pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+char *current_log_entry = NULL;
+size_t current_log_len = 0;
+size_t current_log_cap = 0;
+
+uint64_t global_version = 0;
+unsigned long time_interval_ms;
+
+void handle_sig(int sig, siginfo_t *info, void *context);
+void *client_thread(void *arg);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <TIME_INTERVAL_MS>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    time_interval_ms = strtoul(argv[1], NULL, 10);
+    printf("Server PID: %d\n", getpid());
+
+    global_doc = markdown_init();
+    connected_clients = create_array(8);
+    global_cmd_list = create_array(16);
+    server_log = Calloc(1, 1);
+
+    struct sigaction sa = {0};
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handle_sig;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGRTMIN, &sa, NULL);
+
+    while (1)
+    {
+        usleep(time_interval_ms * 1000);
+
+        pthread_mutex_lock(&cmd_list_mutex);
+        size_t cmd_count = global_cmd_list->size;
+        pthread_mutex_unlock(&cmd_list_mutex);
+
+        if (cmd_count != 0)
+        {
+            reset_log_buffer();
+            current_log_len = snprintf(current_log_entry, current_log_cap,
+                                       "VERSION %llu\n", (unsigned long long)global_version);
+
+            pthread_mutex_lock(&doc_mutex);
+            pthread_mutex_lock(&cmd_list_mutex);
+            for (int i = 0; i < global_cmd_list->size; i++)
+            {
+                cmd_ipc *c = (cmd_ipc *)get_from(global_cmd_list, i);
+                int status = process_raw_command(global_doc, c);
+
+                const char *result_str = NULL;
+
+                switch (status)
+                {
+                case SUCCESS:
+                    result_str = "SUCCESS";
+                    break;
+                case INVALID_CURSOR_POS:
+                    result_str = "REJECT INVALID_POSITION";
+                    break;
+                case DELETED_POSITION:
+                    result_str = "REJECT DELETED_POSITION";
+                    break;
+                case REJECT_UNAUTHORISED:
+                    result_str = "REJECT UNAUTHORISED";
+                    break;
+                default:
+                    result_str = "REJECT UNKNOWN_ERROR";
+                    break;
+                }
+
+                int n = snprintf(NULL, 0, "EDIT %s %s %s\n", c->username, c->raw_command, result_str);
+                char *line = Calloc(n + 1, 1);
+                snprintf(line, n + 1, "EDIT %s %s %s\n", c->username, c->raw_command, result_str);
+                append_to_log_buffer(line, n);
+                free(line);
+            }
+
+            markdown_increment_version(global_doc);
+
+            for (int i = 0; i < global_cmd_list->size; i++)
+            {
+                free_cmd_ipc(get_from(global_cmd_list, i));
+            }
+            global_cmd_list = clear_array(global_cmd_list);
+
+            pthread_mutex_unlock(&cmd_list_mutex);
+            pthread_mutex_unlock(&doc_mutex);
+
+            append_to_log_buffer("END\n", 4);
+
+            append_to_server_log();
+            global_version++;
+        }
+        else
+        {
+
+            reset_log_buffer();
+            current_log_len = snprintf(current_log_entry, current_log_cap,
+                                       "VERSION %llu\nEND\n", (unsigned long long)global_version);
+        }
+
+        send_broadcast_to_all_clients();
+    }
+
+    return 0;
+}
+
+void handle_sig(int sig, siginfo_t *info, void *context)
+{
+    pid_t client_pid = info->si_pid;
+    pthread_t tid;
+    int *arg = Calloc(1, sizeof(int));
+    *arg = client_pid;
+    pthread_create(&tid, NULL, client_thread, arg);
+    pthread_detach(tid);
+}
+
+void *client_thread(void *arg)
+{
+    int client_pid = *((int *)arg);
+    free(arg);
+
+    char fifo_c2s[MAX_FIFO_NAME], fifo_s2c[MAX_FIFO_NAME];
+    snprintf(fifo_c2s, sizeof(fifo_c2s), "FIFO_C2S_%d", client_pid);
+    snprintf(fifo_s2c, sizeof(fifo_s2c), "FIFO_S2C_%d", client_pid);
+
+    unlink(fifo_c2s);
+    unlink(fifo_s2c);
+    mkfifo(fifo_c2s, 0666);
+    mkfifo(fifo_s2c, 0666);
+
+    sigqueue(client_pid, SIGRTMIN + 1, (union sigval){.sival_int = 0});
+
+    int fd_c2s = open(fifo_c2s, O_RDONLY);
+    int fd_s2c = open(fifo_s2c, O_WRONLY);
+
+    char *username = NULL;
+    char *role = NULL;
+    get_user_role(&username, &role, fd_c2s);
+
+    if (!username || !role)
+    {
+        dprintf(fd_s2c, "Reject UNAUTHORISED\n");
+        sleep(1);
+        close(fd_c2s);
+        close(fd_s2c);
+        unlink(fifo_c2s);
+        unlink(fifo_s2c);
+        free(username);
+        free(role);
+        return NULL;
+    }
+
+    dprintf(fd_s2c, "%s\n", role);
+    pthread_mutex_lock(&doc_mutex);
+    dprintf(fd_s2c, "%llu\n", (unsigned long long)global_version);
+    dprintf(fd_s2c, "%zu\n", global_doc->snapshot_len);
+    write(fd_s2c, global_doc->snapshot, global_doc->snapshot_len);
+    pthread_mutex_unlock(&doc_mutex);
+
+    client_info *cinfo = Calloc(1, sizeof(client_info));
+    cinfo->pid = client_pid;
+    cinfo->fd_s2c = fd_s2c;
+    cinfo->username = username;
+    cinfo->permission = role;
+
+    pthread_mutex_lock(&client_list_mutex);
+    append_to(connected_clients, cinfo);
+    pthread_mutex_unlock(&client_list_mutex);
+
+    while (1)
+    {
+        char *line = read_line_dynamic(fd_c2s);
+        if (!line)
+            break;
+
+        if (strcmp(line, "DISCONNECT") == 0)
+        {
+            free(line);
+            break;
+        }
+
+        cmd_ipc *cmd = Calloc(1, sizeof(cmd_ipc));
+        cmd->username = strdup(cinfo->username);
+        cmd->role = strdup(cinfo->permission);
+        cmd->raw_command = line;
+        gettimeofday(&cmd->timestamp, NULL);
+
+        insert_sorted_cmd(cmd);
+    }
+
+    close(fd_c2s);
+    close(fd_s2c);
+    unlink(fifo_c2s);
+    unlink(fifo_s2c);
+
+    pthread_mutex_lock(&client_list_mutex);
+    remove_from(connected_clients, cinfo);
+    pthread_mutex_unlock(&client_list_mutex);
+
+    free(cinfo->username);
+    free(cinfo->permission);
+    free(cinfo);
+
+    return NULL;
+}
